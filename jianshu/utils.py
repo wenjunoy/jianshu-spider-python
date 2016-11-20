f4ace94c74031b8c808b1dfae3637a46c897dbb8
wenjunoy <oy01@mail.ustc.edu.cn>
2016-11-20 19:02:06 +0800

    修改get_articles

--- jianshu/utils.py
+++ jianshu/utils.py
@@ -3,14 +3,15 @@ import logging
 import urllib2
 import requests
 from bs4 import BeautifulSoup
-import sys, os
+import sys, os, re
 
 logger = logging.getLogger(__name__)
 
-def get_content(url):
+def get_content(url, cookie=''):
     headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36",
         'Host' : 'www.jianshu.com',
+        'cookie': cookie
     }
     # logger.info(headers)
     req = urllib2.Request(url = url, headers = headers)
@@ -23,14 +24,13 @@ def get_content(url):
         content = 'FAIL'
     return content
 
-def get_article(url):
+def get_article(content=''):
     '''获取文章的通用函数
     '''
     articles_list = []
-    content = get_content(url)
-    soup = BeautifulSoup(content, 'lxml')
     if content == 'FAIL':
         return articles_list
+    soup = BeautifulSoup(content, 'lxml')
     arts = soup.findAll('h4', attrs={'class':'title'})
     footers = soup.findAll('div', attrs={'class':'list-footer'})
 
@@ -39,7 +39,8 @@ def get_article(url):
     for art, footer in zip(arts, footers):
         # logger.info(art)
         art_id = art.a['href'].replace('/p/','')
-        logger.info(art.a.string)
+        art_name = art.a.string
+        logger.debug(art_name)
 
         a_list = footer.findAll('a')
         views_count = int(re.sub("\D", "", a_list[0].string.strip()))
@@ -60,13 +61,14 @@ def get_collection(url):
     collection_list = []
     content = get_content(url)
     soup = BeautifulSoup(content, 'lxml')
-    collections = soup.findAll('a', attrs={'class':'avatar'})
+    collections = soup.findAll('div', attrs={'class':'collections-info'})
     if not collections:
         return collection_list
     for coll in collections:
-        coll_id = coll['href'].replace('/collection/', '')
-        logger.info(coll_id)
-        collection_list.append(coll_id)
+        coll_id = coll.h5.a['href'].replace('/collection/', '')
+        coll_name = coll.h5.a.string
+        logger.info(coll_name)
+        collection_list.append({'id':coll_id, 'name':coll_name})
     return collection_list
 
 def saveImagesFromUrl(images, filePath):
