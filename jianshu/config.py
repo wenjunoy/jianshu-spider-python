7dd677cb3e9952fa85012f5d18a6eee8927412b4
wenjunoy <oy01@mail.ustc.edu.cn>
2016-12-04 23:26:44 +0800

    update

--- jianshu/config.py
+++ jianshu/config.py
@@ -1,3 +1,3 @@
 #coding:utf-8
 # 登入你的账号，打开个人页F12,查看cookie，复制remember_user_token的内容覆盖如下的xxxxx
-COOKIE = 'remember_user_token=W1sxNDc1NDgyXSwiJDJhJDEwJGU4QjZvS004VjVlZHgyd0o2bkFpWC4iLCIxNDgwODU3ODAzLjUyNzc2MjQiXQ%3D%3D--fd38d5137b11bbd928ec2137374b0208a83c8c5c'
+COOKIE = 'remember_user_token=xxxxx'
